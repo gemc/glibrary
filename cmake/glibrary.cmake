2de4cb2c61621606da422d992e4050a7f4b4d545
Maurizio Ungaro <ungaro@jlab.org>
2022-10-26 16:43:12 -0400

    starting to add glibraries incs and libs

---
+++ cmake/glibrary.cmake
@@ -0,0 +1,28 @@
+include(FetchContent)
+include(CMakePrintHelpers) # to print variables with cmake_print_variables
+
+# Add glibraries dirs to the include path
+
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/eventDispenser)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/textProgressBar)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gdata)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gtranslationTable)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gdynamic)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gtranslationTable)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gtouchable)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gparticle)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/goptions)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/g4system)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gsystem)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gQtButtonsWidget)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gsplash)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gdynamic)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/g4display)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gstreamer)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/guts)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/ghit)
+include_directories(${CMAKE_CURRENT_SOURCE_DIR}/gdata)
+
+
+
+
