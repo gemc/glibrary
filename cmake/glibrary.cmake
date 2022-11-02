6b06d190b6ca13624ee254cf6c237382470a2670
Maurizio Ungaro <ungaro@jlab.org>
2022-11-02 16:34:11 -0400

    added cmakelists

--- cmake/glibrary.cmake
+++ cmake/glibrary.cmake
@@ -4,9 +4,25 @@ include(CMakePrintHelpers) # to print variables with cmake_print_variables
 # Add glibraries dirs to the include path
 
 set(GLIBRARIES_INC_DIRS
-        eventDispenser  textProgressBar  gdata gtranslationTable  gdynamic gtranslationTable
-        gtouchable gparticle  goptions g4system  gsystem gQtButtonsWidget gsplash  gdynamic
-        g4display  gstreamer guts ghit gdata)
+        ${PROJECT_SOURCE_DIR}/eventDispenser
+        ${PROJECT_SOURCE_DIR}/textProgressBar
+        ${PROJECT_SOURCE_DIR}/gdata
+        ${PROJECT_SOURCE_DIR}/gtranslationTable
+        ${PROJECT_SOURCE_DIR}/gdynamic
+        ${PROJECT_SOURCE_DIR}/gtranslationTable
+        ${PROJECT_SOURCE_DIR}/gtouchable
+        ${PROJECT_SOURCE_DIR}/gparticle
+        ${PROJECT_SOURCE_DIR}/goptions
+        ${PROJECT_SOURCE_DIR}/g4system
+        ${PROJECT_SOURCE_DIR}/gsystem
+        ${PROJECT_SOURCE_DIR}/gQtButtonsWidget
+        ${PROJECT_SOURCE_DIR}/gsplash
+        ${PROJECT_SOURCE_DIR}/gdynamic
+        ${PROJECT_SOURCE_DIR}/g4display
+        ${PROJECT_SOURCE_DIR}/gstreamer
+        ${PROJECT_SOURCE_DIR}/guts
+        ${PROJECT_SOURCE_DIR}/ghit
+        ${PROJECT_SOURCE_DIR}/gdata)
 
 
 
